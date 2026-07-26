a6965a5af5bf0370c8ddac35b04995f6c6968d3a
katwalanim6-maker <katwalanim6@gmail.com>
2026-07-26 22:36:36 +0545

    Implement user authentication and logout functionality

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,46 @@
+import { auth, db } from "./firebase.js";
 
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const welcomeText = document.getElementById("welcomeText");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+
+        const data = docSnap.data();
+
+        welcomeText.textContent = `Welcome back, ${data.name} 👋`;
+        username.textContent = data.name;
+        email.textContent = data.email;
+
+    }
+
+});
+
+logoutBtn.addEventListener("click", async () => {
+
+    await signOut(auth);
+
+    window.location.href = "login.html";
+
+});
