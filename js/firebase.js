e2f7e07a4960200979fdef9583d90b0aae38bc03
katwalanim6-maker <katwalanim6@gmail.com>
2026-07-26 23:39:20 +0545

    Refactor Firebase configuration and exports

--- js/firebase.js
+++ js/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByXoBp6vtJJVcXGkUOMnBZwpLX8aBMoG4",
-  authDomain: "anim-os-de470.firebaseapp.com",
-  projectId: "anim-os-de470",
-  storageBucket: "anim-os-de470.firebasestorage.app",
-  messagingSenderId: "164490716722",
-  appId: "1:164490716722:web:07ff8d7a889cac3a9cfe2b"
+  ...
 };
 
 const app = initializeApp(firebaseConfig);
