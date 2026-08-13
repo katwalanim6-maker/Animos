9c1bea8aab13a31e9041cdddb3356a5bbe1546bb
katwalanim6-maker <katwalanim6@gmail.com>
2026-08-13 08:57:03 +0545

    feat: implement shared authentication helpers

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,21 @@
+import { auth } from './firebase.js';
+import { onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 
+export function watchAuth({ requireAuth = false, redirect = 'login.html', onUser } = {}) {
+  return onAuthStateChanged(auth, async (user) => {
+    if (requireAuth && !user) {
+      window.location.href = redirect;
+      return;
+    }
+    if (typeof onUser === 'function') await onUser(user);
+  });
+}
+
+export async function logout(redirect = 'login.html') {
+  await signOut(auth);
+  window.location.href = redirect;
+}
+
+export function requireAuth(redirect = 'login.html', onUser) {
+  return watchAuth({ requireAuth: true, redirect, onUser });
+}
